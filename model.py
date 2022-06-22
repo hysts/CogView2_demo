59c8cca3b87de1416083a478d968782e9c089de6
hysts <hysts@users.noreply.github.com>
2022-06-22 23:05:07 +0000

    Fix typo

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-# This code is adapted frum https://github.com/THUDM/CogView2/blob/4e55cce981eb94b9c8c1f19ba9f632fd3ee42ba8/cogview2_text2image.py
+# This code is adapted from https://github.com/THUDM/CogView2/blob/4e55cce981eb94b9c8c1f19ba9f632fd3ee42ba8/cogview2_text2image.py
 
 from __future__ import annotations
 
