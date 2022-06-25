7b58ca1dc8c685aedd1ccec3ae0b13cde4637ec3
hysts <hysts@users.noreply.github.com>
2022-06-25 11:30:07 +0000

    Change layout

--- app.py
+++ app.py
@@ -73,13 +73,14 @@ def main():
                                            step=1,
                                            value=8,
                                            label='Number of Images')
+                    run_button = gr.Button('Run')
+
                     with open('samples.txt') as f:
                         samples = [
                             line.strip().split('\t') for line in f.readlines()
                         ]
                     examples = gr.Dataset(components=[text, style],
                                           samples=samples)
-                    run_button = gr.Button('Run')
 
             with gr.Column():
                 with gr.Group():
